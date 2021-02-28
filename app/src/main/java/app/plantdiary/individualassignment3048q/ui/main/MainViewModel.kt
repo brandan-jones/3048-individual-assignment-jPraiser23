87b0f4b7bb6148e1f97fd432e5746354a6cdf875
John Praiser <hamedya@mail.uc.edu>
2021-02-28 15:19:39 -0500

    Update Files to New repo

--- app/src/main/java/app/plantdiary/individualassignment3048q/ui/main/MainViewModel.kt
+++ app/src/main/java/app/plantdiary/individualassignment3048q/ui/main/MainViewModel.kt
@@ -1,7 +1,15 @@
 package app.plantdiary.individualassignment3048q.ui.main
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import app.plantdiary.individualassignment3048q.dto.Country
+import service.CountryService
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    var countries: MutableLiveData<ArrayList<Country>> = MutableLiveData<ArrayList<Country>>()
+    var countryService: CountryService = CountryService()
+
+    fun fetchCountries(countryName: String){
+        countries = countryService.fetchCountries(countryName)
+    }
 }
